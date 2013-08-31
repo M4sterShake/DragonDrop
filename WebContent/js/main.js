2db6b2bc6d7266ba714ba73e5ee79509c91de985
M4sterShake <shakerepublic@gmail.com>
2013-08-31 12:28:34 +0100

    First commit
    Just a drag and drop test site for now.

---
+++ WebContent/js/main.js
@@ -0,0 +1,79 @@
+contentViewSelector = "#ContentView";
+
+$(document).ready(function()
+{
+    $(contentViewSelector).on('dragover', function(e)
+    {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    $(contentViewSelector).on('dragleave', function(e)
+    {
+        if ($(e.target).hasClass("dropArea"))
+        {
+            e.preventDefault();
+            e.stopPropagation();
+            $(e.target).css("box-shadow", "inset 0 0 0px 0px #AAA");
+            $(e.target).removeClass("draggedOver");
+        }
+
+    });
+
+    $(contentViewSelector).on('dragenter', function(e)
+    {
+        if ($(e.target).hasClass("dropArea") && !$(e.target).hasClass("draggedOver"))
+        {
+            e.preventDefault();
+            e.stopPropagation();
+            
+            $(e.target).addClass("draggedOver");
+            
+            $(e.target).css("box-shadow", "inset 0 0 0px 0px #AAA");
+            $(e.target).animate(
+            {
+                boxShadow : "inset 0 0 3px 3px #AAA"
+            }, 150);
+        }
+
+    });
+
+    $(contentViewSelector).on('drop', function(e)
+    {
+        var dropArea = getTargetDropArea(e);
+        var droppedFiles = e.originalEvent.dataTransfer.files;
+        $(e.target).css("box-shadow", "inset 0 0 0px 0px #AAA");
+        
+        if (e.originalEvent.dataTransfer)
+        {
+            if (droppedFiles.length)
+            {
+                e.preventDefault();
+                e.stopPropagation();
+                /* UPLOAD FILES HERE */
+                var currDate = new Date();
+                var htmlContentString = "<h3 class='timeStamp'>You uploaded " + droppedFiles.length + " files at " + currDate.toTimeString() + "</h3><hr/><ul>";
+                for ( var i = 0; i < droppedFiles.length; i++)
+                {
+                    var fileNameLi = "<li>" + droppedFiles[i].name + "</li>";
+                    htmlContentString = htmlContentString + fileNameLi;
+                }
+                htmlContentString = htmlContentString + "</ul>";
+                $(dropArea).prepend(htmlContentString);
+            }
+        }
+    });
+
+    function getTargetDropArea(e)
+    {
+        var target;
+        if ($(e.target).hasClass("dropArea"))
+        {
+            target = e.target;
+        } else
+        {
+            target = $(e.target).parents(".dropArea").first();
+        }
+        return target;
+    }
+});// End document ready
---
+++ src/borko/DragonDrop/test.java
@@ -0,0 +1,44 @@
+package borko.DragonDrop;
+
+import java.io.IOException;
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class test
+ */
+@WebServlet("/test")
+public class test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public test() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String title = "Testy Testerson";
+		out.println("<html><head><title>" + title + "</title></head><body><h1>" + title + "</h1></body></html");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
